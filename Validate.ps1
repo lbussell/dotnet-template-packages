19586eee79d994a3511bd1d9480c4a6cfcff9623
Logan Bussell <loganbussell@microsoft.com>
2025-12-20 18:18:58 -0800

    Make validation script use nupkg instead of template directory

--- Validate.ps1
+++ Validate.ps1
@@ -1,6 +1,7 @@
 #!/usr/bin/env pwsh
 
 $artifactsDir = "$PSScriptRoot/artifacts"
+$nupkgDir = "$PSScriptRoot/bin/Release"
 
 # Clear out artifacts directory
 if (Test-Path $artifactsDir) {
@@ -8,8 +9,17 @@ if (Test-Path $artifactsDir) {
 }
 New-Item -ItemType Directory -Path $artifactsDir
 
-# Install the template
-dotnet new install "$PSScriptRoot/src/nuget-package-repo/" --force
+# Clear out any existing nupkgs
+if (Test-Path $nupkgDir) {
+    Remove-Item "$nupkgDir/*.nupkg" -Force
+}
+
+# Pack the template package
+dotnet pack "$PSScriptRoot" -c Release
+
+# Find and install the template package
+$nupkg = Get-ChildItem "$nupkgDir/*.nupkg" | Select-Object -First 1
+dotnet new install $nupkg.FullName --force
 
 # Generate a new project from the template
 dotnet new nuget-package-repo --name TestPackage --output $artifactsDir
